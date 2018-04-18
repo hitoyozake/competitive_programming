8bf451e7e62e18ce5aaea296047763fc7f988095
hitoyozake <mikoto_1122@yahoo.co.jp>
2018-04-18 21:59:10 +0900

    AC

--- yukicoder/47/y47/src/main.rs
+++ yukicoder/47/y47/src/main.rs
@@ -1,3 +1,24 @@
+use std::io::*;
+
+fn read_line()->Result<String>{
+    let mut buffer = String::new();
+
+    let x = stdin().read_line(&mut buffer)?;
+
+    Ok((buffer))
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let num = (String::from(read_line().unwrap().trim())).parse::<u32>().unwrap();
+
+    let mut a = 1;
+    let mut counter = 0;
+
+    while( a < num ){
+        counter += 1;
+        a *= 2;
+    }
+
+    println!("{}", counter);
 }
