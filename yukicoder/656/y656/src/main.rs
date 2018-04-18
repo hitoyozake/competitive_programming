54d56263dd9a7404b40331a021d7800a4ca98d6b
hitoyozake <mikoto_1122@yahoo.co.jp>
2018-04-18 10:49:06 +0900

    str, charsの取扱方法を調べよう;

--- yukicoder/656/y656/src/main.rs
+++ yukicoder/656/y656/src/main.rs
@@ -1,3 +1,21 @@
+use std::io::*;
+use std::str::*;
+
+fn read_line()->Result<String>{
+    let mut buf = String::new();
+
+    let x = stdin().read_line(&mut buf)?;
+
+    return Ok((buf));
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let holes = read_line().unwrap().trim();
+    let mut sum:i32 = 0;
+    for i in holes.chars(){
+        let x = i.to_string();
+        sum += i32::from_str(x).unwrap();
+    }
+    print!("{}", sum);
 }
