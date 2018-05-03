f0cea6a66cfe69b74fe47008dc6a3bb429e23293
hitoyozake <mikoto_1122@yahoo.co.jp>
2018-05-03 21:55:00 +0900

    AC(1)

--- yukicoder/y32/src/main.rs
+++ yukicoder/y32/src/main.rs
@@ -1,3 +1,28 @@
+
+
+
+fn read_line()->String{
+    let mut buf = String::new();
+
+    let x = std::io::stdin().read_line(& mut buf).ok();
+
+    buf
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut n = read_line().trim().parse::<i32>().unwrap();
+    let mut m = read_line().trim().parse::<i32>().unwrap();
+    let mut l = read_line().trim().parse::<i32>().unwrap();
+
+    m = m + l / 25; // 25円玉に1円玉を両替した結果を反映
+    l = l % 25; // 両替したのでその分減らす(あまり)
+    n = n + m/4; // 25*4 -> 100円にする
+    m = m % 4;
+    n = n % 10;
+    //println!("l:{}, m:{}, n:{}", l, m, n);
+
+    println!("{}", l + m + n);
+    
 }
