1fe55adab0afb147e04de55808dfa48e76b1deae
hitoyozake <mikoto_1122@yahoo.co.jp>
2018-05-03 22:00:37 +0900

    AC(2)

--- yukicoder/y32/src/main.rs
+++ yukicoder/y32/src/main.rs
@@ -12,17 +12,15 @@ fn read_line()->String{
 
 
 fn main() {
-    let mut n = read_line().trim().parse::<i32>().unwrap();
-    let mut m = read_line().trim().parse::<i32>().unwrap();
     let mut l = read_line().trim().parse::<i32>().unwrap();
+    let mut m = read_line().trim().parse::<i32>().unwrap();
+    let mut n = read_line().trim().parse::<i32>().unwrap();
 
-    m = m + l / 25; // 25円玉に1円玉を両替した結果を反映
-    l = l % 25; // 両替したのでその分減らす(あまり)
-    n = n + m/4; // 25*4 -> 100円にする
-    m = m % 4;
-    n = n % 10;
-    //println!("l:{}, m:{}, n:{}", l, m, n);
+    let x = l*100 + m*25 + n;
 
-    println!("{}", l + m + n);
+    let result = (x % 1000) / 100 + ((x % 1000) % 100) / 25 + (((x%1000)%100)%25); 
+    
+
+    println!("{}", result);
     
 }
