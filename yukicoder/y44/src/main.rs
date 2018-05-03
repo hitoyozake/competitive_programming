50db36f7072e1b016a8d8bd230e3eccb3da33a04
hitoyozake <mikoto_1122@yahoo.co.jp>
2018-05-03 17:35:07 +0900

    AC

--- yukicoder/y44/src/main.rs
+++ yukicoder/y44/src/main.rs
@@ -1,3 +1,43 @@
+use std::collections::HashMap;
+
+fn read_line()->String{
+    let mut buf = String::new();
+
+    let x = std::io::stdin().read_line(&mut buf).ok();
+    
+    buf
+}
+
+
+fn fibo(n:i64, map:&mut HashMap<i64, i64>)->i64
+{
+    let mut r:i64 = 0;
+    match map.get(&n)
+    {
+        Some(&number) => {
+            r = number;
+        },
+        _ => { 
+            let num = fibo(n-1, map) + fibo(n-2, map);
+            map.insert(n, num);
+            r = num;
+        },
+    }
+    r   
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut map:HashMap<i64, i64> = HashMap::new();
+
+    let n = read_line().trim().parse::<i64>().unwrap();
+
+    map.insert(1, 1);
+    map.insert(2,1);
+
+    let num = fibo(n+1, &mut map);
+
+    println!("{}", num);
+
+
+    
 }
