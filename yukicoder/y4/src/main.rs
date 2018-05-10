f232b44b2d62322112d5deadb957199ed00960b7
hitoyozake <mikoto_1122@yahoo.co.jp>
2018-05-11 07:09:02 +0900

    AC

--- yukicoder/y4/src/main.rs
+++ yukicoder/y4/src/main.rs
@@ -18,13 +18,83 @@ fn get_bitcount(x:u32)->u32{
     count
 }
 
-
-
 fn main() {
-    
-    for i in 0..32{
-        println!("{}:{}", i, get_bitcount(i));
+    let n = read_line().trim().parse::<u32>().unwrap();
+
+    let mut queue: std::collections::VecDeque<u32> = std::collections::VecDeque::new();
+    let mut map: std::collections::HashMap<u32,u32> = std::collections::HashMap::new();
+    queue.push_front(1);
+    let mut current:u32 = 1;
+    let mut step:u32 = 0;
+
+    map.insert(1, 1);
+
+    while(queue.is_empty() == false){
+        current = queue.pop_back().unwrap();
+         match(map.get(&current)){
+            Some(_found) =>{
+                step = *_found;
+            },
+            _ => {
+                // never reach
+            },
+        };
+        if current == n{
+            break;
+        }
+
+        let bitcount = get_bitcount(current);
+
+        //get next
+        let next = current + bitcount;
+        let mut found = false;
+        match(map.get(&next)){
+            Some(_found) =>{
+                found = true;
+            },
+            _ => {
+                
+            },
+        };
+
+        if found == false{
+            map.insert(next, step+1);
+                if next <= n {
+                    queue.push_front(next);    
+                }
+        }
+
+        let next = current - bitcount;
+        let mut found = false;
+
+        match(map.get(&next)){
+            Some(_found) =>{
+                found = true;
+            },
+            _ => {
+            },
+        };
+
+
+        if found == false{
+            map.insert(next, step+1);
+                if next <= n {
+                    queue.push_front(next);    
+                }
+        }
     }
 
-    println!("Hello, world!");
+    let mut r = -1;
+
+    match(map.get(&n)){
+        Some(_found)=>{
+            r = *_found as i64;
+        },
+        _ =>{
+
+        },
+    };
+    
+
+    println!("{}", r);
 }
