656be014de1b5c7ddaa8b662d9e2ba9de3fb5e85
hitoyozake <mikoto_1122@yahoo.co.jp>
2018-05-10 12:01:38 +0900

    bitcount関数を作成

--- yukicoder/y4/src/main.rs
+++ yukicoder/y4/src/main.rs
@@ -1,3 +1,30 @@
+
+fn read_line()->String{
+    let mut buf = String::new();
+
+    let x = std::io::stdin().read_line(&mut buf);
+
+    return buf;
+}
+
+fn get_bitcount(x:u32)->u32{
+    let mut tmp = x;
+    let mut count = 0;
+    for i in 0..16{
+        count += tmp % 2;
+
+        tmp = tmp >> 1;
+    }
+    count
+}
+
+
+
 fn main() {
+    
+    for i in 0..32{
+        println!("{}:{}", i, get_bitcount(i));
+    }
+
     println!("Hello, world!");
 }
