475a2449c5dc052205ec92b911e49b91eca7eb20
hitoyozake <mikoto_1122@yahoo.co.jp>
2018-05-20 01:04:59 +0900

    AC ナップサックdp．ちょうどを作る方法

--- yukicoder/y3/src/main.rs
+++ yukicoder/y3/src/main.rs
@@ -1,3 +1,29 @@
+
+
+fn read_line()->String{
+    let mut buf = String::new();
+
+    let x = std::io::stdin().read_line(&mut buf);
+
+    buf
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut queue: std::collections::VecDeque<u32> = std::collections::VecDeque::new();
+
+    let n = read_line().trim().parse::<u32>().unwrap();
+
+    let s = String::from(read_line().trim());
+    let v = s.split(" ");
+
+    queue.push_back(v.front().parse::<u32>().unwrap());
+
+    for i in v{
+        let mut tmpv = std::collections::Vec::new();
+
+        while(!queue.empty()){
+            tmpv.push_back(queue.pop());
+        }
+    }
+    
 }
