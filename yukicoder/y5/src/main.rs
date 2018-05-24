6ef0afe2274ddbc0b6cef23a040b4713c0b82912
hitoyozake <mikoto_1122@yahoo.co.jp>
2018-05-25 07:06:13 +0900

    変数の準備

--- yukicoder/y5/src/main.rs
+++ yukicoder/y5/src/main.rs
@@ -1,3 +1,25 @@
+
+fn read_line()->String{
+    let mut buf = String::new();
+
+    let x = std::io::stdin().read_line(&mut buf);
+
+    buf
+}
+
+
 fn main() {
+
+    let max:i32 = read_line().trim().parse::<i32>().unwrap();
+
+    let n:i32 = read_line().trim().parse::<i32>().unwrap();
+
+    let blocks = String::from(read_line().trim());
+
+    let v = blocks.split(" ");
+
+    let num_vect: Vec<i32> = Vec::new();
+
+
     println!("Hello, world!");
 }
