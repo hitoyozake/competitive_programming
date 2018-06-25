550060399d207e5ad6a4f62fa876ad2d3ce6795c
hitoyozake <mikoto_1122@yahoo.co.jp>
2018-06-26 05:58:55 +0900

    rustに移植

--- yukicoder/y9/src/main.rs
+++ yukicoder/y9/src/main.rs
@@ -1,3 +1,32 @@
+use std::collections::BinaryHeap;
+
+struct Unit{
+    level: i32,
+    count: i32
+}
+
+fn get_line()->String{
+    let mut buf = String::new();
+
+    let r = std::io::stdin().get_line(mut & buf);
+
+    buf
+}
+
+
 fn main() {
+
+    struct Inches(i32);
+
+    let length = Inches(10);
+
+    let Inches(integer_length) = length;
+    let il:Inches = Inches(20);
+
+    let mut heap:BinaryHeap<i32>= BinaryHeap::new();
+
+
+
+
     println!("Hello, world!");
 }
