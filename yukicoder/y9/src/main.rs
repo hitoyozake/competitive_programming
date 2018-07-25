d2181fe87d68d49e3f2d85e6a15198dba886b420
hitoyozake <mikoto_1122@yahoo.co.jp>
2018-07-25 23:08:06 +0900

    function実装

--- yukicoder/y9/src/main.rs
+++ yukicoder/y9/src/main.rs
@@ -15,21 +15,24 @@ fn get_line()->String{
 
 use std::cmp::Ordering;
 
+//#[derive(Eq, Clone)]
 #[derive(Eq)]
-struct Monster{
+struct Mnt{
     level: i32,
     count: i32
 }
 
 
-impl Clone for Monster{
-    fn clone(&self)->Monster{
-        Monster{level:self.level, count:self.count}
+// コピーを実装する
+impl Clone for Mnt{
+    fn clone(&self)->Mnt{
+        Mnt{level:self.level, count:self.count}
     }
 }
 
-impl PartialOrd for Monster{
-    fn partial_cmp(&self, other: &Monster)->Option<Ordering>{
+// 比較方法を実装する
+impl PartialOrd for Mnt{
+    fn partial_cmp(&self, other: &Mnt)->Option<Ordering>{
         if self.level < other.level
         {
             Some(Ordering::Greater)
@@ -44,16 +47,16 @@ impl PartialOrd for Monster{
 }
 
 
-impl PartialEq for Monster{
-    fn eq(&self, other: &Monster)->bool{
+impl PartialEq for Mnt{
+    fn eq(&self, other: &Mnt)->bool{
         (self.level, self.count) == (other.level, other.count)
     }
 
 }
 
 // BinaryHeapのために実装
-impl Ord for Monster{
-    fn cmp(&self, other: &Monster)->Ordering{
+impl Ord for Mnt{
+    fn cmp(&self, other: &Mnt)->Ordering{
         
         (self.level, self.count).cmp(&(other.level, other.count))
         //(self.level, self.count).cmp(&(other.level, other.count))
@@ -69,26 +72,26 @@ fn main() {
     let Inches(integer_length) = length;
     let il:Inches = Inches(20);
 
-    let mut que:BinaryHeap<Monster>= BinaryHeap::new();
+    let mut que:BinaryHeap<Mnt>= BinaryHeap::new();
 
     let n: i32 = get_line().trim().parse::<i32>().unwrap();
 
     let tmp = String::from(get_line().trim());
     let sv:Vec<&str> = tmp.split(" ").collect();
 
-    let mut party: Vec<Monster> = Vec::new();
+    let mut party: Vec<Mnt> = Vec::new();
 
     for i in sv{
-        que.push(Monster{ level:i.parse::<i32>().unwrap(), count: 0});
+        que.push(Mnt{ level:i.parse::<i32>().unwrap(), count: 0});
     }
 
     let tmp = String::from(get_line().trim());
     let sv:Vec<&str> = tmp.split(" ").collect();
 
-    let mut enem_levels: Vec<i32> = Vec::new();
+    let mut el: Vec<i32> = Vec::new();
 
     for i in sv{
-        enem_levels.push(i.parse::<i32>().unwrap()/2);
+        el.push(i.parse::<i32>().unwrap()/2);
     }
 
     let mut max_count_min = 1000000;
@@ -97,7 +100,7 @@ fn main() {
         let mut pt = que.clone();
 
         let mut max_count = 0;
-        //popされた時の値をうまくなるように調整が必要そう
+        
         for j in 0..n{
                 let mut selected = pt.pop().unwrap();
                 //println!("selected . l:{}, c:{}", selected.level, selected.count);
@@ -105,7 +108,7 @@ fn main() {
 
                 let usz = ((i+j)%n) as u32;
 
-                selected.level += enem_levels[usz as usize];
+                selected.level += el[usz as usize];
                 max_count = std::cmp::max(max_count, selected.count);
                 //println!("r . l:{}, c:{}", selected.level, selected.count);
         
