85dc497e8e9768514b4914d6643c65a344c54480
hitoyozake <mikoto_1122@yahoo.co.jp>
2018-08-18 16:50:16 +0900

    hashmap

--- yukicoder/y29/src/main.rs
+++ yukicoder/y29/src/main.rs
@@ -1,3 +1,23 @@
+
+fn get_line()->String{
+    let mut buf = String::new();
+    let x = std::io::stdin().read_line(& mut buf);
+    buf
+}
+
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+
+    let n = get_line().trim().parse::<u64>().unwrap();
+    let mut hashMap: HashMap<u64, i64> = HashMap::new();
+    for i in 0..n{
+        let s = get_line();
+        let items = s.trim().split(' ');
+
+        for item in items{
+            let i:u64 = item.parse::<u64>().unwrap();
+            *hashMap.entry(i).or_insert(0) += 1;
+        }
+    }
 }
