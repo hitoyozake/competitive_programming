a3af2ce9f5f6c0a25de81d940ffd9771210d0a3c
hitoyozake <mikoto_1122@yahoo.co.jp>
2020-03-31 22:42:32 +0900

    commit

---
+++ atcoder/ABC/043/b.rs
@@ -0,0 +1,57 @@
+
+
+// 標準入力一行を読み取り，指定の型に変換する関数
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+
+fn read_splited<T: std::str::FromStr>() -> Vec<T>{
+
+    let mut s = String::new();
+
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().split_whitespace().map(|e| e.parse().ok().unwrap()).collect()
+
+}
+
+
+
+
+fn main(){
+
+    let s: String = read();
+    let mut v = Vec::new();
+
+    for c in s.chars() {
+        match c {
+        '0' => {
+            v.push(c);
+        },
+        '1' => {
+            v.push(c);
+        }
+        'B' => {
+            if v.len() > 0{
+                v.pop();
+            }
+        }
+        _ => println!("ERROR!")
+
+        }
+
+    }
+
+
+    for i in v {
+        print!("{}", i);
+    }
+    print!("\n");
+    //let f = format!("{}", s);
+
+
+    //println!("{}", f);
+}
+
