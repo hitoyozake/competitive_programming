b5c8d5e297c565adf089fb84f81c489482759b82
hitoyozake <mikoto_1122@yahoo.co.jp>
2020-11-17 05:31:53 +0900

    mmm

---
+++ atcoder/ABC/300points/c61/src/main.rs
@@ -0,0 +1,47 @@
+use std::collections::HashMap;
+
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+
+
+fn main() {
+    let (n, k) = {
+        let s = read::<String>();
+        let s2:Vec<&str> = s.split(' ').collect();
+        let mut iter = s2.iter();
+        (iter.next().unwrap().parse::<i64>().unwrap(), iter.next().unwrap().parse::<i64>().unwrap())
+    };
+
+    let mut hash:HashMap<i64, i64> = HashMap::new();
+
+    for i in 0..n {
+        let (x, y) = {
+            let s = read::<String>();
+            let s2:Vec<&str> = s.split(' ').collect();
+            let mut iter = s2.iter();
+            (iter.next().unwrap().parse::<i64>().unwrap(), iter.next().unwrap().parse::<i64>().unwrap())
+        };
+        let m = hash.entry(x).or_insert(0);
+        *m += y;
+    }
+
+    let mut counter = 0;
+
+    //dbg!(n, k);
+
+    for (key, value) in hash{
+        
+        //dbg!(num);
+        counter += value;
+
+        if counter >= k{
+
+            println!("{}", key);
+            break;
+        }
+        //dbg!(counter);
+    }
+}
