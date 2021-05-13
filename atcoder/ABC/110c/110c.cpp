65af93fc5742787c36c315c8a1984f26a102a907
hitoyozake <mikoto_1122@yahoo.co.jp>
2021-05-13 20:35:47 +0900

    AC

--- atcoder/ABC/110c/110c.cpp
+++ atcoder/ABC/110c/110c.cpp
@@ -1,3 +1,38 @@
 #include <iostream>
 #include <string>
 #include <map>
+
+int main(){
+    std::string s1, s2;
+    std::cin >> s1;
+    std::cin >> s2;
+
+    std::map<char, char> dict1, dict2;
+    for(int i = 0; i < s1.size(); ++i){
+        {
+            const auto c = dict1[s1[i]];
+            if(c == '\0'){
+                dict1[s1[i]] = s2[i];
+            }
+            else if(c != s2[i]){
+                std::cout << "No" << std::endl;
+                return 0;
+            }
+        }
+        {
+            const auto c = dict2[s2[i]];
+            if(c == '\0'){
+                dict2[s2[i]] = s1[i];
+            }
+            else if(c != s1[i]){
+                std::cout << "No" << std::endl;
+                return 0;
+            }
+        }
+    }
+
+    std::cout << "Yes" << std::endl;
+
+    return 0;
+    
+}
--- atcoder/ABC/abc73c/src/main.rs
+++ atcoder/ABC/abc73c/src/main.rs
@@ -1,3 +1,16 @@
+use std::collections::HashMap;
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+
 fn main() {
+    let num: String = read();
+    let mut map: HashMap<i64> = HashMap::new();    
+    for i in 0..num {
+
+    }
+
     println!("Hello, world!");
 }
