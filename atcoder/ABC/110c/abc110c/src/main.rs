c897a690eb7ba8a559178531a3f37db2c7293f17
hitoyozake <mikoto_1122@yahoo.co.jp>
2021-05-15 12:40:30 +0900

    commit

--- atcoder/ABC/110c/abc110c/src/main.rs
+++ atcoder/ABC/110c/abc110c/src/main.rs
@@ -1,3 +1,48 @@
+use std::collections::HashMap;
+
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let s1: Vec<char>  = read::<String>().chars().collect();
+    let s2: Vec<char> = read::<String>().chars().collect();
+
+    let mut dict1: HashMap<char, char>  = HashMap::new();
+    let mut dict2: HashMap<char, char>  = HashMap::new();
+
+    //&char, &char 
+    for (i, j) in s1.iter().zip(s2.iter()) {
+        match dict1.get(i){
+            Some(v) => {
+                if v != j{
+                    println!("No");
+                    return;
+                }
+            
+            } ,
+            None => {
+                // 値を入れる
+                dict1.insert(*i, *j);
+            }
+        }
+        match dict2.get(j){
+            Some(v) => {
+                if v != i{
+                    println!("No");
+                    return;
+                }
+            },
+            None => {
+                // 値を入れる
+                dict2.insert(*j, *i);
+            }
+        }
+    }
+
+    println!("Yes");
+
 }
